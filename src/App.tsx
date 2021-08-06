00fd7268842ed3ac6ec4497517add405f4b843da
Thang <viktor.thang.phan@gmail.com>
2021-08-06 12:38:01 -0400

    Change in reaction of bar numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,76 @@
-import React from "react";
+import React, { FormEvent, useEffect } from "react";
 import "./App.css";
 import { getMergeSortAnimation } from "./MergeSortAnimation";
 import { getBubbleSortAnimation } from "./BubbleSortAnimations";
 
-const BAR_NUMBER = 100;
+const BAR_NUMBER = 30;
 const SPEED = 1;
-const FIRST_COLOR = '#2E2FE3'
-const SECOND_COLOR = '#FDED2A'
+const FIRST_COLOR = "#2E2FE3";
+const SECOND_COLOR = "#FDED2A";
+
+const MAXBAR = 420;
 class App extends React.Component<{}, any> {
   constructor(props: any) {
     super(props);
 
     this.state = {
       array: [],
+      barNum: 40,
+      barWidth: 25,
     };
   }
-  resetArray() {
+  resetArray(newInput: number) {
     const array = [];
-    for (let i = 0; i < BAR_NUMBER; i++) {
-      array.push(randomIntFromInterval(5, 700));
+    for (let i = 0; i < newInput; i++) {
+      array.push(randomIntFromInterval(5, 690));
     }
 
     this.setState({ array });
   }
   componentDidMount() {
-    this.resetArray();
+    this.resetArray(this.state.barNum);
   }
+
+  handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const newInput = parseInt(e.currentTarget.value);
+    console.log(newInput);
+    if (newInput > MAXBAR) {
+      this.setState({ barNum: MAXBAR });
+      // this.resetArray();
+      console.log(this.state.barNum);
+    } else {
+      this.setState({
+        barNum: newInput,
+      });
+    }
+    this.resetArray(newInput);
+  };
+  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    if (this.state.barNum > 340) {
+      this.setState({ barWidth: 2 });
+    } else if (this.state.barNum > 240) {
+      this.setState({ barWidth: 3 });
+    } else if (this.state.barNum > 180) {
+      this.setState({ barWidth: 5 });
+    } else if (this.state.barNum > 140) {
+      this.setState({ barWidth: 7 });
+    } else if (this.state.barNum > 100) {
+      this.setState({ barWidth: 10 });
+    } else if (this.state.barNum > 60) {
+      this.setState({ barWidth: 15 });
+    }
+    //Ask Phuc why cannot I set the maximum
+    if (this.state.barNum > 420) {
+      this.setState({ barNum: MAXBAR });
+      this.componentDidMount();
+    } else {
+      this.setState({
+        barNum: this.state.barNum,
+      });
+    }
+    e.preventDefault();
+    this.resetArray(parseInt(this.state.barNum));
+  };
   mergeSortAnimation = () => {
     const animations = getMergeSortAnimation(this.state.array);
     //animations length  = 3x array length, 0 change color, 1 change color back, 2 swap value;
@@ -88,31 +133,46 @@ class App extends React.Component<{}, any> {
   };
 
   render() {
-    const { array } = this.state;
-    console.log(array);
+    const { array, barNum } = this.state;
+
     return (
       <div className="App">
         <div className="options">
-          <button onClick={() => this.resetArray()}>Reset Chart</button>
+          <button onClick={() => this.resetArray(barNum)}>Reset Chart</button>
           <button onClick={() => this.mergeSortAnimation()}>Merge sort</button>
           <button onClick={() => this.bubbleSortAnimation()}>
             Bubble sort
           </button>
+          <div className="forms">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="number"
+                name="barNumber"
+                onChange={this.handleChange}
+                value={this.state.barNum}
+              />
+              <button type="submit">Bars</button>
+            </form>
+          </div>
         </div>
         <div className="array-box">
-          {array.map((value: number, idx: number) => (
-            <div
-              className="bar"
-              key={idx}
-              style={{ height: `${value}px` }}
-            ></div>
-          ))}
+          {barNum > 0 &&
+            array.map((value: number, idx: number) => (
+              <div
+                className="bar"
+                key={idx}
+                style={{
+                  height: `${value}px`,
+                  width: `${this.state.barWidth}px`,
+                }}
+              ></div>
+            ))}
         </div>
       </div>
     );
   }
 }
-function randomIntFromInterval(min: number, max: number) {
+const randomIntFromInterval = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1 + min));
-}
+};
 export default App;
